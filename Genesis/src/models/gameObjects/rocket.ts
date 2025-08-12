247bec26656fcb32f3fb78ba8433a8367063769e
PinSopheaktra <sopheakpin342@gmail.com>
2025-08-12 23:13:42 +0700

    add draw thruster

--- Genesis/src/models/gameObjects/rocket.ts
+++ Genesis/src/models/gameObjects/rocket.ts
@@ -1,3 +1,4 @@
+import { Color } from "../color";
 import { GameObjectBase } from "./gameObjectBase";
 
 export class Rocket extends GameObjectBase {
@@ -6,6 +7,9 @@ export class Rocket extends GameObjectBase {
     height: number = 50;
     physicBody: Phaser.Physics.Matter.Sprite;
     thrustForce: number = 0.003;
+    isThrusting: boolean = false;
+    isThrustingRight: boolean = false;
+    isThrustingLeft: boolean = false;
 
     constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, x, y);
@@ -23,15 +27,34 @@ export class Rocket extends GameObjectBase {
     }
 
     public drawRocket(): void {
-        const color = 0xff0000; // Red color for the rocket
+        this.clear();
+        const color = 0xff0000;
         const alpha = 1;
 
-        // Draw the body of the rocket
         this.drawRectangle(-this.width / 2, -this.height / 2, this.width, this.height, color, alpha);
 
-        // Draw the nose cone of the rocket
-        this.fillStyle(0x000000, alpha); // Black color for the nose cone
+        this.fillStyle(0x000000, alpha);
         this.fillTriangle(0, -this.height / 2, -this.width / 2, -this.height / 2 + this.height / 4, this.width / 2, -this.height / 2 + this.height / 4);
+
+        if(this.isThrusting){
+            this.drawThrust();
+        }
+        if(this.isThrustingLeft){
+            this.drawThrustRight();
+        }
+        if(this.isThrustingRight){
+            this.drawThrustLeft();
+        }
+    }
+    public drawThrust(): void {
+        this.fillStyle(0xffa500, 1); // Orange color for the thrust
+        this.fillTriangle(0, this.height / 2, -this.width / 4, this.height / 2 + this.height / 4, this.width / 4, this.height / 2 + this.height / 4);
+    }
+    public drawThrustLeft(): void{
+        
+    }
+    public drawThrustRight(): void{
+
     }
 
     override update(...args: any[]): void {
@@ -39,15 +62,28 @@ export class Rocket extends GameObjectBase {
         this.y = this.physicBody.y;
         this.rotation = this.physicBody.rotation;
         super.update(...args);
-        //this.drawRocket();
+        this.drawRocket();
+        this.isThrusting = false;
+        this.isThrustingLeft = false;
+        this.isThrustingRight = false;
     }
+
+    //#region Rocket Controls
     handleThrust() {
-        this.physicBody.thrustLeft(this.thrustForce);;
+        this.physicBody.thrustLeft(this.thrustForce);
+        this.isThrusting = true; // Set thrusting state to true
     }
     stearRight() {
         this.physicBody.setAngularVelocity(this.physicBody.getAngularVelocity()+this.thrustForce); // Adjust the angular velocity for right steering
+        this.isThrustingRight = true;
     }
     stearLeft() {
         this.physicBody.setAngularVelocity(this.physicBody.getAngularVelocity()-this.thrustForce); // Adjust the angular velocity for left steering
+        this.isThrustingLeft = true;
     }
+    //#endregion Rocket Controls
+
+    //#region Rocket sensors
+    
+    //#endregion Rocket sensors
 }
