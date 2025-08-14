da09a395d2ca00d00b43f38baee1321830d17e78
PinSopheaktra <sopheakpin342@gmail.com>
2025-08-14 16:15:41 +0700

    draw proximetry sensor

--- Genesis/src/models/gameObjects/gameObjectBase.ts
+++ Genesis/src/models/gameObjects/gameObjectBase.ts
@@ -1,8 +1,11 @@
 export class GameObjectBase extends Phaser.GameObjects.Graphics {
+    vectorHandler: Phaser.Math.Vector2; 
 
     constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, {x, y});
+        this.vectorHandler = new Phaser.Math.Vector2();
         scene.add.existing(this);
+
     }
 
     drawRectangle(x: number, y: number, width: number, height: number, color: number = 0xffffff, alpha: number = 1): void {
@@ -28,6 +31,14 @@ export class GameObjectBase extends Phaser.GameObjects.Graphics {
         this.fillStyle(color, alpha);
         this.fill();
         this.closePath();
+    }
+    drawLineByAngle(x: number, y: number, length: number, angle: number, color: number, alpha: number = 1) {
+        const radians = Phaser.Math.DegToRad(angle);
+        this.vectorHandler.setToPolar(radians, length);
+        const x2 = x + this.vectorHandler.x;
+        const y2 = y + this.vectorHandler.y;
+
+        this.drawLine(x, y, x2, y2, color, alpha);
     }
 
     override update(...args: any[]): void {
--- Genesis/src/models/gameObjects/rocket.ts
+++ Genesis/src/models/gameObjects/rocket.ts
@@ -5,16 +5,33 @@ export class Rocket extends GameObjectBase {
     
     width: number = 20;
     height: number = 50;
+
+    readonly rocketLeftEdge: number;
+    readonly rocketRightEdge: number;
+    readonly rocketUpHalf: number;
+    readonly rocketTop: number;
+    readonly rocketBottom: number;
+
     physicBody: Phaser.Physics.Matter.Sprite;
     thrustForce: number = 0.003;
     isThrusting: boolean = false;
     isThrustingRight: boolean = false;
     isThrustingLeft: boolean = false;
-    sensor: RocketSensor = new RocketSensor();
+    sensor: RocketSensor;
 
     constructor(scene: Phaser.Scene, x: number, y: number) {
         super(scene, x, y);
+
         this.generateTexture('rocket');
+
+        this.rocketLeftEdge = -this.width / 2;
+        this.rocketRightEdge = this.width / 2;
+        this.rocketUpHalf = -this.height / 4;
+        this.rocketTop = -this.height / 2;
+        this.rocketBottom = this.height / 2;
+
+        this.sensor = new RocketSensor();
+
         this.drawRocket();
 
         this.physicBody = scene.matter.add.sprite(x, y, 'rocket', '', {
@@ -41,39 +58,44 @@ export class Rocket extends GameObjectBase {
         this.fillTriangle(0, rockety, rocketx, rockety + this.height / 4, this.width / 2, rockety + this.height / 4);
 
         if(this.isThrusting){
-            this.drawThrust(rocketx,rockety);
+            this.drawThrust();
         }
         if(this.isThrustingLeft){
-            this.drawThrustRight(rocketx,rockety);
+            this.drawThrustRight();
         }
         if(this.isThrustingRight){
-            this.drawThrustLeft(rocketx,rockety);
+            this.drawThrustLeft();
         }
+
+        this.sensor.drawFronProximitySensors(this);
+        this.sensor.drawFronLeftProximitySensors(this);
+        this.sensor.drawFronRightProximitySensors(this);
+
+        this.sensor.drawLeftProximitySensors(this);
+        this.sensor.drawRightProximitySensors(this);
+
+        this.sensor.drawRearProximitySensors(this);
+        this.sensor.drawRearLeftProximitySensors(this);
+        this.sensor.drawRearRightProximitySensors(this);
     }
-    public drawThrust(rocketx: number,rockety: number): void {
+    public drawThrust(): void {
         this.fillStyle(0xffa500, 1);
         const thrustLength = this.width;
-        const rocketMidX = rocketx + (this.width/2);
-        const rocketBottum = rockety + this.height;
-        const rocketLeftEch = rocketx;
-        const rocketRightEch = rocketx + this.width;
-        this.fillTriangle(rocketMidX, rocketBottum, rocketLeftEch, rocketBottum+thrustLength, rocketRightEch, rocketBottum+thrustLength);
+        const rocketMidX = 0;
+        const rocketBottum = this.height/2;
+        this.fillTriangle(rocketMidX, rocketBottum, this.rocketLeftEdge, rocketBottum+thrustLength, this.rocketRightEdge, rocketBottum+thrustLength);
     }
-    public drawThrustLeft(rocketx: number,rockety: number): void{
+    public drawThrustLeft(): void{
         this.fillStyle(Color.GetHexFromRGB(255,255,255), 1);
-        const rocketLeftEch = rocketx;
-        const rocketUpHalf = rockety + (this.height/4);
         const thrustLength = this.width/2;
         const thrustRedius = (this.width/4);
-        this.fillTriangle(rocketLeftEch, rocketUpHalf, rocketLeftEch-thrustLength, rocketUpHalf+thrustRedius, rocketLeftEch-thrustLength, rocketUpHalf-thrustRedius)
+        this.fillTriangle(this.rocketLeftEdge, this.rocketUpHalf, this.rocketLeftEdge-thrustLength, this.rocketUpHalf+thrustRedius, this.rocketLeftEdge-thrustLength, this.rocketUpHalf-thrustRedius)
     }
-    public drawThrustRight(rocketx: number,rockety: number): void{
+    public drawThrustRight(): void{
         this.fillStyle(Color.GetHexFromRGB(255,255,255), 1);
-        const rocketRightEch = rocketx + this.width;
-        const rocketUpHalf = rockety + (this.height/4);
         const thrustLength = this.width/2;
         const thrustRedius = (this.width/4);
-        this.fillTriangle(rocketRightEch, rocketUpHalf, rocketRightEch+thrustLength, rocketUpHalf+thrustRedius, rocketRightEch+thrustLength, rocketUpHalf-thrustRedius)
+        this.fillTriangle(this.rocketRightEdge, this.rocketUpHalf, this.rocketRightEdge+thrustLength, this.rocketUpHalf+thrustRedius, this.rocketRightEdge+thrustLength, this.rocketUpHalf-thrustRedius)
     }
 
     override update(...args: any[]): void {
@@ -108,7 +130,12 @@ export class Rocket extends GameObjectBase {
 }
 
 export class RocketSensor{
+    readonly proximitySensorsColor: number;
     readonly defaultProximitySensorsValue: number = 100;
+
+    constructor() {
+        this.proximitySensorsColor = Color.GetHexFromRGB(255, 255, 255);
+    }
 
     fronLeftProximitySensors: number = this.defaultProximitySensorsValue;
     fronProximitySensors: number = this.defaultProximitySensorsValue;
@@ -120,4 +147,38 @@ export class RocketSensor{
     rearLeftProximitySensors: number = this.defaultProximitySensorsValue;
     rearProximitySensors: number = this.defaultProximitySensorsValue;
     rearRightProximitySensors: number = this.defaultProximitySensorsValue;
+
+    gyroscope: number = 0;
+
+    ySpeedometer: number = 0;
+    xSpeedometer: number = 0;
+
+    excalorator: number = 0;
+
+    drawFronProximitySensors(rocket: Rocket){
+        rocket.drawLineByAngle(0, rocket.rocketTop, this.fronProximitySensors, -90, this.proximitySensorsColor, 0.5);
+    }
+    drawFronLeftProximitySensors(rocket: Rocket){
+        rocket.drawLineByAngle(rocket.rocketLeftEdge, rocket.rocketTop, this.leftProximitySensors, -(90+45), this.proximitySensorsColor, 0.5);
+    }
+    drawFronRightProximitySensors(rocket: Rocket){
+        rocket.drawLineByAngle(rocket.rocketRightEdge, rocket.rocketTop, this.rightProximitySensors, -45, this.proximitySensorsColor, 0.5);
+    }
+
+    drawLeftProximitySensors(rocket: Rocket){
+        rocket.drawLineByAngle(rocket.rocketLeftEdge, 0, this.leftProximitySensors, -180, this.proximitySensorsColor, 0.5);
+    }
+    drawRightProximitySensors(rocket: Rocket){
+        rocket.drawLineByAngle(rocket.rocketRightEdge, 0, this.rightProximitySensors, 0, this.proximitySensorsColor, 0.5);
+    }
+
+    drawRearProximitySensors(rocket: Rocket){
+        rocket.drawLineByAngle(0, rocket.rocketBottom, this.rearProximitySensors, 90, this.proximitySensorsColor, 0.5);
+    }
+    drawRearLeftProximitySensors(rocket: Rocket){
+        rocket.drawLineByAngle(rocket.rocketLeftEdge, rocket.rocketBottom, this.rearLeftProximitySensors, 90+54, this.proximitySensorsColor, 0.5);
+    }
+    drawRearRightProximitySensors(rocket: Rocket){
+        rocket.drawLineByAngle(rocket.rocketRightEdge, rocket.rocketBottom, this.rearRightProximitySensors, 45, this.proximitySensorsColor, 0.5);
+    }
 }
