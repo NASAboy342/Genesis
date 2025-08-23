b40b9838d85053ee3863393d62de4f50737affe3
NASAboy342 <sopheakpin342@gmail.com>
2025-08-23 15:15:54 +0700

    add cycle count

--- Genesis/src/view/game.vue
+++ Genesis/src/view/game.vue
@@ -19,15 +19,21 @@ const frameRate = ref(0);
 let mapWidth: number = 2000;
 let mapHeight: number = 1000;
 
-const bestNeuralNetworkJson = ref(router.currentRoute.value.query.neuralNetworkInJsonString as string || '');
+const bestNeuralNetworkJson = ref(
+  (router.currentRoute.value.query.neuralNetworkInJsonString as string) || ""
+);
+const cyclesCompleted = ref(0);
 
 const copyBestNeuralNetwork = () => {
-  if (!bestNeuralNetworkJson.value || bestNeuralNetworkJson.value === '') {
+  if (!bestNeuralNetworkJson.value || bestNeuralNetworkJson.value === "") {
     return;
   }
-    navigator.clipboard.writeText(bestNeuralNetworkJson.value).then(() => {
+  navigator.clipboard
+    .writeText(bestNeuralNetworkJson.value)
+    .then(() => {
       console.log("Neural network copied to clipboard");
-    }).catch(err => {
+    })
+    .catch((err) => {
       console.error("Failed to copy: ", err);
     });
 };
@@ -35,13 +41,13 @@ const copyBestNeuralNetwork = () => {
 class GameScene extends Phaser.Scene {
   backgroundGrid: GridBackground;
   ground: Ground;
-  rockets: Rocket[] = [];  
+  rockets: Rocket[] = [];
   gridSize: number = 50;
   cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
   matterCategory: MatterCategory;
   rocketWayPoint: WayPoint;
   clock: Clock;
-  highestScoredRocketNeuralNetWork: string = bestNeuralNetworkJson.value || '';
+  highestScoredRocketNeuralNetWork: string = bestNeuralNetworkJson.value || "";
   cameraXVelocity: number = 0;
   cameraYVelocity: number = 0;
 
@@ -53,9 +59,20 @@ class GameScene extends Phaser.Scene {
 
   create() {
     this.matterCategory = new MatterCategory(this);
-    this.backgroundGrid = new GridBackground(this, mapWidth, mapHeight, this.gridSize);
+    this.backgroundGrid = new GridBackground(
+      this,
+      mapWidth,
+      mapHeight,
+      this.gridSize
+    );
     this.setCollisionToWorldBounds();
-    this.ground = new Ground(this, mapWidth / 2, mapHeight, mapWidth, this.matterCategory);
+    this.ground = new Ground(
+      this,
+      mapWidth / 2,
+      mapHeight,
+      mapWidth,
+      this.matterCategory
+    );
     this.spawnRockets();
     this.rocketWayPoint = new WayPoint(this, mapWidth / 4, mapHeight / 3);
     this.cursorKeys = this.input.keyboard.createCursorKeys();
@@ -63,59 +80,77 @@ class GameScene extends Phaser.Scene {
   }
   setCollisionToWorldBounds() {
     const walls = this.matter.world.walls;
-    [walls.left, walls.right, walls.top, walls.bottom].forEach((w: MatterJS.BodyType) => {
-      w.collisionFilter.category = this.matterCategory.static;
-      w.collisionFilter.mask = this.matterCategory.rocket; 
-    });
+    [walls.left, walls.right, walls.top, walls.bottom].forEach(
+      (w: MatterJS.BodyType) => {
+        w.collisionFilter.category = this.matterCategory.static;
+        w.collisionFilter.mask = this.matterCategory.rocket;
+      }
+    );
   }
   spawnRockets() {
-    let rocketStartPoint = new Phaser.Math.Vector2(mapWidth / 1.2, mapHeight- 70)
-    for(let i = 0; i < 100; i++) {
-      this.rockets.push(new Rocket(this, rocketStartPoint.x, rocketStartPoint.y, this.matterCategory, this.highestScoredRocketNeuralNetWork === '' ? '' : this.highestScoredRocketNeuralNetWork));
+    let rocketStartPoint = new Phaser.Math.Vector2(
+      mapWidth / 1.2,
+      mapHeight - 70
+    );
+    for (let i = 0; i < 100; i++) {
+      this.rockets.push(
+        new Rocket(
+          this,
+          rocketStartPoint.x,
+          rocketStartPoint.y,
+          this.matterCategory,
+          this.highestScoredRocketNeuralNetWork === ""
+            ? ""
+            : this.highestScoredRocketNeuralNetWork
+        )
+      );
     }
   }
 
   //#region Update
   update() {
-    this.checkIfToRecycleGame()
+    this.checkIfToRecycleGame();
     this.listenForInput();
     this.scrollCamera();
     let surfaces = this.getInteractiveSerfaces();
-    this.rockets.forEach(rocket => {
+    this.rockets.forEach((rocket) => {
       rocket.update(surfaces, this.rocketWayPoint);
     });
     frameRate.value = this.game.loop.actualFps;
     this.clock.aging();
   }
-  
+
   //#endregion Update
   checkIfToRecycleGame() {
-    if(this.clock.ageInSec > 5){
+    if (this.clock.ageInSec > 5) {
       this.extractHighestScoredRocketNeuralNetwork();
-      this.rockets.forEach(rocket => {
+      this.rockets.forEach((rocket) => {
         rocket.destroy(true);
       });
       this.rockets = [];
       this.spawnRockets();
       this.clock.reset();
+      cyclesCompleted.value += 1;
     }
   }
   extractHighestScoredRocketNeuralNetwork() {
-    this.highestScoredRocketNeuralNetWork = serializeNeuralNetwork(this.rockets.sort((a, b) => b.score - a.score)[0].neuralNetwork);
+    this.highestScoredRocketNeuralNetWork = serializeNeuralNetwork(
+      this.rockets.sort((a, b) => b.score - a.score)[0].neuralNetwork
+    );
     bestNeuralNetworkJson.value = this.highestScoredRocketNeuralNetWork;
   }
-  
+
   listenForInput() {
-    if(this.cursorKeys.up.isDown){
+    if (this.cursorKeys.up.isDown) {
       this.pendingCameraUp();
     }
-    if(this.cursorKeys.left.isDown){
+    if (this.cursorKeys.left.isDown) {
       this.pendingCameraLeft();
     }
-    if(this.cursorKeys.right.isDown){
+    if (this.cursorKeys.right.isDown) {
       this.pendingCameraRight();
     }
-    if(this.cursorKeys.down.isDown){
+    if (this.cursorKeys.down.isDown) {
       this.pendingCameraDown();
     }
   }
@@ -140,11 +175,15 @@ class GameScene extends Phaser.Scene {
     this.cameraYVelocity -= 1;
   }
   getInteractiveSerfaces(): Phaser.Geom.Line[] {
-    let interactiveSerfaces : Phaser.Geom.Line[] = [];
+    let interactiveSerfaces: Phaser.Geom.Line[] = [];
     // world bounds
     interactiveSerfaces.push(new Phaser.Geom.Line(0, 0, mapWidth, 0)); // top
-    interactiveSerfaces.push(new Phaser.Geom.Line(mapWidth, 0, mapWidth, mapHeight)); // right
-    interactiveSerfaces.push(new Phaser.Geom.Line(mapWidth, mapHeight, 0, mapHeight)); // bottom
+    interactiveSerfaces.push(
+      new Phaser.Geom.Line(mapWidth, 0, mapWidth, mapHeight)
+    ); // right
+    interactiveSerfaces.push(
+      new Phaser.Geom.Line(mapWidth, mapHeight, 0, mapHeight)
+    ); // bottom
     interactiveSerfaces.push(new Phaser.Geom.Line(0, mapHeight, 0, 0)); // left
 
     // ground
@@ -170,18 +209,18 @@ onMounted(() => {
       forceSetTimeOut: true, // Use setTimeout for frame rate control
     },
     physics: {
-          default: "matter", // ✅ use Matter.js
-          matter: {
-            gravity: { x: 0, y: 1}, // normal downward gravity
-            debug: false,
-            setBounds: {
-              x: 0,
-              y: 0,
-              width: mapWidth,
-              height: mapHeight,
-            }
-          }
+      default: "matter", // ✅ use Matter.js
+      matter: {
+        gravity: { x: 0, y: 1 }, // normal downward gravity
+        debug: false,
+        setBounds: {
+          x: 0,
+          y: 0,
+          width: mapWidth,
+          height: mapHeight,
         },
+      },
+    },
     scene: GameScene,
   });
 });
@@ -193,9 +232,12 @@ onUnmounted(() => {
 
 <template>
   <div id="game-container"></div>
-  <div class="fps-display">
-    FPS: {{ frameRate }}
-    <div class="copy-button" @click="copyBestNeuralNetwork">Copy best Neural in Json</div>
+  <div class="info-panel">
+    <div class="fps-display">FPS: {{ frameRate }}</div>
+    <div class="fps-display">CompletedCycles: {{ cyclesCompleted }}</div>
+    <div class="copy-button" @click="copyBestNeuralNetwork">
+      Copy best Neural in Json
+    </div>
   </div>
 </template>
 
@@ -208,15 +250,13 @@ onUnmounted(() => {
 }
 .fps-display {
   position: relative;
-  top: 50px;
-  left: 10px;
-  background-color: transparent;;
+  background-color: transparent;
   color: white;
 }
 .copy-button {
   position: relative;
   width: fit-content;
-  background-color: #4CAF50;
+  background-color: #4caf50;
   color: white;
   padding: 10px;
   border-radius: 5px;
@@ -224,5 +264,14 @@ onUnmounted(() => {
 }
 .copy-button:hover {
   background-color: #45a049;
+}
+.info-panel {
+  position: absolute;
+  top: 50px;
+  left: 10px;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 }
 </style>
