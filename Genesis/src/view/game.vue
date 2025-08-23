e5938984239b04565dd6972b92f737c515ad9489
NASAboy342 <sopheakpin342@gmail.com>
2025-08-23 21:34:36 +0700

    increase recycle time

--- Genesis/src/view/game.vue
+++ Genesis/src/view/game.vue
@@ -122,7 +122,7 @@ class GameScene extends Phaser.Scene {
 
   //#endregion Update
   checkIfToRecycleGame() {
-    if (this.clock.ageInSec > 10) {
+    if (this.clock.ageInSec > 20) {
       this.extractHighestScoredRocketNeuralNetwork();
       this.rockets.forEach((rocket) => {
         rocket.destroy(true);
